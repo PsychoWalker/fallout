47bc29f99826156e5886e08b6f508aecafc7d6ae
aleksandr_fomin <a.fomin@prof-p.ru>
2023-07-25 14:49:55 +0300

    добавил страницу команды

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {readyAnimationHandler} from "./readyAnimation.js";
 import {conceptHandler} from "./concept.js";
+import {teamHandler} from "./team";
 
 readyAnimationHandler();
 conceptHandler();
+teamHandler();
---
+++ js/team.js
@@ -0,0 +1,20 @@
+const teamHandler = () => {
+    const team = document.querySelector('.team');
+    const goBack = document.querySelector('.goBack');
+    const screenFirst = document.querySelector('.screen-1')
+    const screenThird = document.querySelector('.screen-3')
+
+    team.addEventListener('click', (e) => {
+        e.preventDefault();
+        screenFirst.classList.add('right');
+        screenThird.classList.add('main');
+    });
+
+    goBack.addEventListener('click', (e) => {
+        e.preventDefault();
+        screenFirst.classList.remove('right');
+        screenThird.classList.remove('main');
+    });
+}
+
+export {teamHandler}
